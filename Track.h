580aff0ff9f3694cefd647437c87928682436c13
AlexKellogg <166714076+AlexKellogg@users.noreply.github.com>
2026-03-23 20:29:04 -0400

    Create Track.h

---
+++ Track.h
@@ -0,0 +1,51 @@
+//
+// Created by alexk on 3/22/2026.
+//
+
+#ifndef TRACK_H
+#define TRACK_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Track {
+    int index = -1;
+
+    string track_id;
+    string track_name;
+    string artists;
+    string album_name;
+    string track_genre;
+
+    int popularity = 0;
+    int duration_ms = 0;
+    bool explicit_flag = false;
+
+    double danceability = 0.0;
+    double energy = 0.0;
+    double loudness = 0.0;
+    double speechiness = 0.0;
+    double acousticness = 0.0;
+    double instrumentalness = 0.0;
+    double liveness = 0.0;
+    double valence = 0.0;
+    double tempo = 0.0;
+
+    vector<double> featureVector() const {
+        return {
+            danceability,
+            energy,
+            speechiness,
+            acousticness,
+            instrumentalness,
+            liveness,
+            valence,
+            tempo,
+            loudness
+        };
+    }
+};
+
+#endif
